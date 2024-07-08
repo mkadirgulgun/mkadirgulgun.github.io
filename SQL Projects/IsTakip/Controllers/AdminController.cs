f3e1053d67f5ec54d1191de0e7e0126b9622f6e0
mkadirgulgun <164685182+mkadirgulgun@users.noreply.github.com>
2024-07-09 01:31:14 +0300

    Add files via upload

---
+++ SQL Projects/IsTakip/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using IsTakip.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Reflection;
+
+namespace IsTakip.Controllers
+{
+    public class AdminController : Controller
+    {
+        string connectionString = "TrustServerCertificate=True";
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddJob()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddJob(JobModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.MessageCssClass = "alert-danger";
+                ViewBag.Message = "Eksik veya hatalı işlem yaptın";
+                return View("Message");
+            }
+            using var connection = new SqlConnection(connectionString);
+            var ilanlar = "INSERT INTO jobs (Person, JobTitle, JobDetail, StatusId) VALUES (@Person, @JobTitle, @JobDetail, @Status)";
+            
+            var data = new
+            {
+                model.Person,
+                model.JobTitle,
+                model.JobDetail,
+                model.Status,
+            };
+
+            var rowsAffected = connection.Execute(ilanlar, data);
+            
+            ViewBag.MessageCssClass = "alert-success";
+            ViewBag.Message = "Eklendi.";
+            ViewBag.Return = "IlanEkle";
+            return View("Message");
+        }
+    }
+}
---
+++ SQL Projects/IsTakip/Models/JobModel.cs
@@ -0,0 +1,17 @@
+﻿namespace IsTakip.Models
+{
+    public class JobModel
+    {
+        public int Id { get; set; }
+        public string Person { get; set; }
+        public string JobTitle { get; set; }    
+        public string JobDetail { get; set; }
+        public DateTime Date { get; set; }  
+        public string? Active { get; set; }
+        public string? Completed { get; set; }
+        public string? Total { get; set; }
+        public string Status { get; set; }
+        public int StatusId { get; set; }   
+    }
+    
+}
